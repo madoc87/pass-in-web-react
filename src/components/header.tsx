c182945edb2a41d1cebfae983e7b8046dbf64c54
Fabrício Assis <madoc87@gmail.com>
2024-04-24 16:40:43 -0300

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import codeIcon from  '../assets/code-icon.svg'
+import { NavLink } from './nav-link'
+
+export function Header() {
+    return (
+        <div className='flex items-center gap-5 py-2'>
+            <img src={codeIcon} alt="" />
+
+            <nav className='flex items-center gap-5'>
+                <NavLink href='/eventos'>Eventos</NavLink>
+                <NavLink href='/participantes'>Participantes</NavLink>
+            </nav>
+        </div>
+    )
+}
